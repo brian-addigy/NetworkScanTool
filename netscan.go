168289948591ad08feaebb71dcd5079617833b14
Brian Delgado <brian@addigy.com>
2020-04-17 09:29:57 -0400

    Fixed stdout Formatting Added signed binary files Logged (data/time) all output Implemented version check

--- netscan.go
+++ netscan.go
@@ -3,7 +3,7 @@ package main
 import (
 	"bytes"
 	"encoding/json"
-	"fmt"
+	"github.com/olekukonko/tablewriter"
 	flag "github.com/spf13/pflag"
 	"io"
 	"log"
@@ -14,7 +14,6 @@ import (
 	"regexp"
 	"strconv"
 	"strings"
-	"text/tabwriter"
 )
 
 type host struct {
@@ -22,74 +21,87 @@ type host struct {
 	OsType  string `json:"os_type"`
 	IpAddr  string `json:"ip_address"`
 	MacAddr string `json:"mac_address"`
-	ports   string `json:"open_ports"`
+	Ports   string `json:"open_ports"`
+	Hostname string `json:"hostname"`
 }
+
+const (
+	version = "1.0"
+)
 
 func main() {
 	var allHosts []host
 	nmapPath := "/Library/Addigy/nmap"
 	ipAddrs := GetIpAddress()
 	jsonFlagPtr := flag.BoolP("json","j",false,"Sets out to json format")
-	scanFlagPtr := flag.BoolP("scan", "s", false, "scans the network for hosts and their open ports")
+	scanFlagPtr := flag.BoolP("scan", "s", false, "scans the network for hosts and their open Ports")
+	versionFlagPtr := flag.BoolP("version", "v", false, "Displays currently version of Netscan")
 	flag.Parse()
 
-	// Checks if nmap; downloads if not
-	_, pathErr := os.Stat(nmapPath)
-	if pathErr != nil {
-		fmt.Println("File does not exist.")
-		InstallNmap(nmapPath)
+	if *versionFlagPtr {
+		log.Printf("Netscan v%s\n", version)
+		return
 	}
+
 
 	// --scan/-s isnt provided, print usage
 	if !*scanFlagPtr {
 		flag.Usage()
-	} else {
-		// Find local ip Address
-		if ipAddrs != "error" {
-			fmt.Println("Beginning Scan .....................\n")
-			allHosts = ParseData(GetNmapData(nmapPath, ipAddrs))
-			// flag is set
-			if *jsonFlagPtr  {
-				JsonPrintHosts(allHosts)
-			} else {
-				PrintHosts(allHosts)
-			}
+		return
+	}
+
+	// Checks if nmap; downloads if not
+	_, pathErr := os.Stat(nmapPath)
+	if pathErr != nil {
+		log.Println("Nmap does not exist.")
+		InstallNmap(nmapPath)
+	}
+
+	// Find local ip Address
+	if ipAddrs != "error" {
+		log.Println("Beginning Scan .....................\n")
+		allHosts = ParseData(GetNmapData(nmapPath, ipAddrs))
+		// flag is set
+		if *jsonFlagPtr  {
+			JsonPrintHosts(allHosts)
+		} else {
+			PrintHosts(allHosts)
 		}
 	}
 }
 
 func InstallNmap(nmapPath string) {
 	url := "https://s3.amazonaws.com/files.addigy.com/nmap"
-	fmt.Println("Now Installing nmap")
+	log.Println("Now Installing nmap")
 
 	// Create blank file
 	out, createErr := os.Create(nmapPath)
 	if createErr != nil {
-		fmt.Println("Error creating file path: ", createErr)
+		log.Println("Error creating file path: ", createErr)
 	}
 	defer out.Close()
 
 	// Get the data
 	resp, urlErr := http.Get(url)
 	if urlErr != nil {
-		fmt.Println("Error retreiving nmap: ", urlErr)
+		log.Println("Error retreiving nmap: ", urlErr)
 	}
 	defer resp.Body.Close()
 
 	// Write the data to file
 	_, copyErr := io.Copy(out, resp.Body)
 	if copyErr != nil {
-		fmt.Println("Error installing nmap: ", copyErr)
+		log.Println("Error installing nmap: ", copyErr)
 	}
 
 	// Make it executable
 	exeCmd := exec.Command("chmod", "+x", nmapPath)
 	runErr := exeCmd.Run()
 	if runErr != nil {
-		fmt.Println("Error running nmap: ", runErr)
+		log.Println("Error running nmap: ", runErr)
 	}
 
-	fmt.Println("Done installing nmap")
+	log.Println("Done installing nmap")
 }
 
 func GetNmapData(nmapPath string,ip string) []string {
@@ -113,7 +125,7 @@ func GetIpAddress() string {
 	addrs, err := net.InterfaceAddrs()
 
 	if err != nil {
-		fmt.Println(err)
+		log.Println(err)
 	}
 	//Loop through network interfaces
 	for _, address := range addrs {
@@ -131,17 +143,26 @@ func GetIpAddress() string {
 	return "error"
 }
 
+func ParseHostname(hostname string) string {
+	if hostname == "" {
+		return "Unknown Hostname"
+	}
+	return hostname
+}
+
 func ParseData(data []string) []host {
-	macAdrReg := regexp.MustCompile(`([0-9a-fA-F][0-9a-fA-F]:){5}([0-9a-fA-F][0-9a-fA-F])`)
-	ipAdrReg := regexp.MustCompile(`(?:[0-9]{1,3}\.){3}[0-9]{1,3}`)
-	brandReg := regexp.MustCompile(`\((.*?)\)`)
-	portReg := regexp.MustCompile(`(open)`)
+	macAdrRegEx := regexp.MustCompile(`([0-9a-fA-F][0-9a-fA-F]:){5}([0-9a-fA-F][0-9a-fA-F])`)
+	ipAdrRegEx := regexp.MustCompile(`(?:[0-9]{1,3}\.){3}[0-9]{1,3}`)
+	hostnameRegEx := regexp.MustCompile(`\s(.*?[.com])\s`)
+	brandRegEx := regexp.MustCompile(`\((.*?)\)`)
+	portRegEx := regexp.MustCompile(`(open)`)
 	var allHosts []host
 	var curHost host
 
 	// iterate through entire data
 	for i:=1; i < len(data); i++ {
-		ipAdrs := ipAdrReg.FindString(data[i])
+		ipAdrs := ipAdrRegEx.FindString(data[i])
+		hostname := hostnameRegEx.FindString(data[i])
 
 		// iterate through 1 host
 		// if scan reports <ipaddress>, then a new host has appeared
@@ -149,21 +170,22 @@ func ParseData(data []string) []host {
 			// reuse host
 			curHost = host{}
 			curHost.IpAddr = ipAdrs
+			curHost.Hostname = ParseHostname(hostname)
 		}
 		// Port header is found, port numbers follow
 		if strings.Contains(data[i-1],"PORT") {
-			// if i-1 contains port then the ports are at i
+			// if i-1 contains port then the Ports are at i
 
-			// iterate through all ports, as long as you dont find end of port (contains "MAC" or a blank space))
+			// iterate through all Ports, as long as you dont find end of port (contains "MAC" or a blank space))
 			for data[i] != "" && !strings.Contains(data[i], "MAC") {
 				portNum := strings.Split(data[i]," ")
-				portSrv :=  portReg.FindString(data[i])
+				portSrv :=  portRegEx.FindString(data[i])
 				// found a Regular exp. match
 				if portSrv != ""  {
-					if curHost.ports == "" {
-						curHost.ports = portNum[0]
+					if curHost.Ports == "" {
+						curHost.Ports = portNum[0]
 					} else {
-						curHost.ports = curHost.ports + ", " + portNum[0]
+						curHost.Ports = curHost.Ports + ", " + portNum[0]
 					}
 				}
 				// Continue to iterate
@@ -171,11 +193,12 @@ func ParseData(data []string) []host {
 			}
 			// host ends with MAC address; except last host (current device)
 			if strings.Contains(data[i],"MAC ") {
-				mac := macAdrReg.FindString(data[i])
-				brand := brandReg.FindString(data[i])
+				mac := macAdrRegEx.FindString(data[i])
+				brand := brandRegEx.FindString(data[i])
 				curHost.MacAddr = mac
-				curHost.Brand = brand
-				curHost.OsType = GetOs(curHost.ports,curHost.Brand)
+				curHost.Brand = strings.Trim(brand,"(")
+				curHost.Brand = strings.Trim(curHost.Brand,")")
+				curHost.OsType = GetOs(curHost.Ports,curHost.Brand)
 				// add current host to the list of hosts
 				allHosts = append(allHosts, curHost)
 			}
@@ -187,11 +210,8 @@ func ParseData(data []string) []host {
 func GetOs(ports string, brand string) string {
 	iosPort := "62078"
 
-	// found an apple product
 	if strings.Contains(brand, "Apple") {
-		// check if its iOS, if not default to MacOS
 		if strings.Contains(ports, iosPort) {
-			// added space for formatting issue. All are 7 spaces long
 			return "iOS"
 		} else {
 			return "MacOS"
@@ -201,25 +221,22 @@ func GetOs(ports string, brand string) string {
 }
 
 func PrintHosts(allHosts []host) {
-	writer := new(tabwriter.Writer)
-	writer.Init(os.Stdout, 0, 8, 1, '\t', tabwriter.Debug|tabwriter.AlignRight|tabwriter.TabIndent)
-	fmt.Fprintln(writer, "Manufacturer\tOS type\tMAC Address\tIP Address\tOpen ports")
-	fmt.Fprintln(writer, "----------------------\t-------\t-------------------\t--------------\t--------------")
+	table := tablewriter.NewWriter(os.Stdout)
+	table.SetHeader([]string{"Manufacturer","Hostname","MAC Address","IP Address","OS type","Open Ports"})
+	table.SetBorder(false)                                // Set Border to false
 
 	// print out each host
 	for i:=0; i < len(allHosts); i++ {
-		fmt.Fprintf(writer, "%s\t%s\t%s\t%s\t%s\n", allHosts[i].Brand,allHosts[i].OsType,allHosts[i].MacAddr, allHosts[i].IpAddr,allHosts[i].ports)
+		table.Append([]string{allHosts[i].Brand,allHosts[i].Hostname,allHosts[i].MacAddr, allHosts[i].IpAddr,allHosts[i].OsType,allHosts[i].Ports})
 	}
-	fmt.Fprintln(writer)
-	writer.Flush()
-
-	fmt.Println("Scan Complete.")
-	fmt.Println("Total number of Hosts up:", len(allHosts))
+	table.Render()
+	log.Println("\nScan Complete.")
+	log.Println("Total number of Hosts up:", len(allHosts))
 }
 
 func JsonPrintHosts(allHost []host) {
 	// convert struct(s) to json format
 	jsonHosts, _ := json.MarshalIndent(allHost, "", " ")
 	// write out json to file
-	fmt.Println(string(jsonHosts))
+	log.Println(string(jsonHosts))
 }
